c319f848679871fb83e8b4a84bd8e8d315aa3b79
JR <jr@upzelo.com>
2023-12-04 09:49:52 +0000

    Add DataFields->createDataField() resource

--- src/V2/Resources/DataFields.php
+++ src/V2/Resources/DataFields.php
@@ -6,6 +6,7 @@ namespace Dotdigital\V2\Resources;
 
 use Dotdigital\Resources\AbstractResource;
 use Dotdigital\V2\Models\DataFieldList;
+use Dotdigital\V2\Models\DataField;
 
 class DataFields extends AbstractResource
 {
@@ -19,5 +20,25 @@ class DataFields extends AbstractResource
     public function show()
     {
         return new DataFieldList($this->get(self::RESOURCE_BASE));
+    }
+
+    /**
+     * @return DataField
+     * @throws \Http\Client\Exception
+     * @throws \Exception
+     */
+    public function createDataField(string $name, string $type): DataField
+    {
+        $response = $this->post(
+            self::RESOURCE_BASE,
+            [
+                "name" => $name,
+                "type" => $type,
+                "visibility" => 'Private',
+                "defaultValue" => ''
+            ]
+        );
+
+        return new DataField($response);
     }
 }
